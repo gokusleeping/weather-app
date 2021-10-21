75a1c7f76fb4f1df6e5497cbfcb905fbe1ab5edb
Ranvir Singh <gokusleeping@gmail.com>
2021-10-21 21:37:50 +0530

    second commit

--- pages/weather.jsx
+++ pages/weather.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-//import "./weather.css";
-
 const Weather = (props) => {
   return (
-    <div className="container text-light">
+    <div className="container text-light d-flex justify-content-center">
       <div className="Card">
         <h1 className="text-white py-3">{props.cityname}</h1>
-        <h5 className="py-4">
+        <h5 className="py-4 d-flex justify-content-center">
           <i className={`wi ${props.weatherIcon} display-1`} />
         </h5>
 
         {/* Get Celsius */}
         {props.temp_celsius ? (
-          <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+          <h1 className="py-2 d-flex justify-content-center">
+            {props.temp_celsius}&deg;
+          </h1>
         ) : null}
 
         {/* show max and min temp */}
